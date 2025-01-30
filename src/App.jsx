a42c73a27186d77298507caa8e7d42de85db913e
Finanzasparahippies <a210202086@unison.mx>
2025-01-30 11:21:48 -0700

    set interval added

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import { ReactFlowComponent } from './components/';
 import logo from './assets/img/logo.png';
 import { ReactFlowProvider } from '@xyflow/react';
@@ -7,6 +8,17 @@ import LoginPage from './views/LoginPage';
 import PrivateRoute from './components/routes/PrivateRoute';
 
 const App = () => {
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      fetch('https://gvhc-backend.onrender.com/')
+          .then(response => console.log('Manteniendo activo:', response.status))
+          .catch(error => console.error('Error manteniendo activo:', error));
+  }, 600000); // 10 minutos
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <NotesProvider>
         <ReactFlowProvider>
